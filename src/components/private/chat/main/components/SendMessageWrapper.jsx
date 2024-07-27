3278766a33c2a47f972f6259c0d7fce2547e9593
Luis Diaz <v-lureynoso@expediagroup.com>
2024-07-26 19:27:27 -0700

    Message creation with image

--- src/components/private/chat/main/components/SendMessageWrapper.jsx
+++ src/components/private/chat/main/components/SendMessageWrapper.jsx
@@ -1,10 +1,14 @@
 import { useEffect, useState } from 'react';
 import MessageInput from 'components/MessageInput';
+import FileUploaderInput from 'components/FileUploaderInput';
+import ImageUploaderInput from 'components/ImageUploaderInput';
 import { useChat, useDispatchChat } from 'providers/chat';
 import { useAuth } from 'providers/auth';
+import { objectToFormData } from 'utils/serializer';
 
 export default () => {
     const [messageText, setMessageText] = useState('');
+    const [messageFile, setMessageFile] = useState('');
     const chat = useChat();
     const auth = useAuth();
     const dispatchChat = useDispatchChat();
@@ -20,36 +24,42 @@ export default () => {
 
     const sendMessage = (e) => {
         e.preventDefault();
-        dispatchChat({ type: 'http/sendMessage', payload: {
-            sender: auth.user,
-            receiver: chat.activeContact,
-            message: messageText
-        }});
+
+        const file = messageFile || '';
+        let formData = new FormData();
+        formData.append('message', messageText);
+        formData.append('file', file);
+        formData = objectToFormData('sender', auth.user, formData);
+        formData = objectToFormData('receiver', chat.activeContact, formData);
+
+        dispatchChat({ type: 'http/sendMessage', payload: formData });
     }
+
+    // TO-DO: Restrict file upload and preview to be files only.
+    // TO-DO: Image upload should only select image files from the file system and / or open the camera.
+    const onFileUpload = (e) => {
+        if (e.target.files.length !== 0) {
+            const file = e.target.files[0];
+            setMessageFile(file);
+        }
+
+        // Display file in UI
+        // const reader = new FileReader();
+        // reader.onload = () => {
+        //     setLoadImage(reader.result);
+        // };
+        // reader.readAsDataURL(e.target.files[0]);
+    };
 
     return (
         <footer className="flex justify-between items-center w-full shadow py-2 md:p-3 bg-zinc-100 dark:bg-gray-800 dark:border-r dark:border-t dark:border-slate-700">
             <div className="flex items-center gap-2">
-                <svg className="size-7 transition-all
-                        text-sky-700
-                        cursor-pointer
-                        hover:text-sky-600
-                        dark:text-sky-600
-                        dark:hover:text-sky-700
-                        " xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-                    <path fillRule="evenodd" d="M5.625 1.5H9a3.75 3.75 0 0 1 3.75 3.75v1.875c0 1.036.84 1.875 1.875 1.875H16.5a3.75 3.75 0 0 1 3.75 3.75v7.875c0 1.035-.84 1.875-1.875 1.875H5.625a1.875 1.875 0 0 1-1.875-1.875V3.375c0-1.036.84-1.875 1.875-1.875ZM12.75 12a.75.75 0 0 0-1.5 0v2.25H9a.75.75 0 0 0 0 1.5h2.25V18a.75.75 0 0 0 1.5 0v-2.25H15a.75.75 0 0 0 0-1.5h-2.25V12Z" clipRule="evenodd" />
-                    <path d="M14.25 5.25a5.23 5.23 0 0 0-1.279-3.434 9.768 9.768 0 0 1 6.963 6.963A5.23 5.23 0 0 0 16.5 7.5h-1.875a.375.375 0 0 1-.375-.375V5.25Z" />
-                </svg>
-                <svg className="size-7 transition-all
-                        text-sky-700
-                        cursor-pointer
-                        hover:text-sky-600
-                        dark:text-sky-600
-                        dark:hover:text-sky-700
-                        " xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-                    <path d="M12 9a3.75 3.75 0 1 0 0 7.5A3.75 3.75 0 0 0 12 9Z" />
-                    <path fillRule="evenodd" d="M9.344 3.071a49.52 49.52 0 0 1 5.312 0c.967.052 1.83.585 2.332 1.39l.821 1.317c.24.383.645.643 1.11.71.386.054.77.113 1.152.177 1.432.239 2.429 1.493 2.429 2.909V18a3 3 0 0 1-3 3h-15a3 3 0 0 1-3-3V9.574c0-1.416.997-2.67 2.429-2.909.382-.064.766-.123 1.151-.178a1.56 1.56 0 0 0 1.11-.71l.822-1.315a2.942 2.942 0 0 1 2.332-1.39ZM6.75 12.75a5.25 5.25 0 1 1 10.5 0 5.25 5.25 0 0 1-10.5 0Zm12-1.5a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z" clipRule="evenodd" />
-                </svg>
+                <FileUploaderInput 
+                    onChangeHandler={onFileUpload}
+                />
+                <ImageUploaderInput
+                    onChangeHandler={onFileUpload}
+                />
             </div>
             <div className="grow px-2">
                 <MessageInput
